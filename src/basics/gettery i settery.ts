f6897807fecbd7c593e6a5d1397ef1c2537ccc05
Esochi <sochiera.emilia@gmail.com>
2023-03-25 20:00:36 +0100

    gettery i settery

---
+++ src/basics/gettery i settery.ts
@@ -0,0 +1,8 @@
+// metody zaczynające się słowem kluczowym get to gettery odwołujemy się do nich jak do zwykłych pól. Wazne jest, ze gettery są tylko do odczytu wiec nie mozemy im przypisywac innych wartosci
+
+get lifeEvents(): string [] {
+    return this.lifeEventsList.filter(s => s !== 'Kompromitacja');
+}
+ const bartek9= new Human ('Bartek', 'B.');
+
+ //Settery pozwalją zapisywać (ustawiać) nowe wartości.
