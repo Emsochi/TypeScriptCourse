b7a4974aff73aa8704d9e5b45425ebb373f90a39
Esochi <sochiera.emilia@gmail.com>
2023-03-25 20:04:50 +0100

    abstract classes

---
+++ src/basics/abstract classes.ts
@@ -0,0 +1,9 @@
+// są podobne do interfejsów. Stanowią pewnego rodzaju szablon jednak nie mona tworzyć obiektów na podstawie klasy abstrakcyjnej
+
+abstract class Animal10 {
+    constructor(
+        private readonly specieName: string
+    ){}
+}
+
+const duck = new Animal10 ('duck')
