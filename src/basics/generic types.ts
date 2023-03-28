9f55fc49a9445b199d3ce3e5f3fa8c8a4b7387ca
Esochi <sochiera.emilia@gmail.com>
2023-03-28 22:20:53 +0200

    generic types

---
+++ src/basics/generic types.ts
@@ -0,0 +1,5 @@
+//Typ generyczny to taki, który moze mieć subtyp, czyli moe być związany z innym typem. 
+//Przykładowo Array jest typem generycznym (ogolnym), a szczegółowym podtypem moze być np.number
+
+const ar: Array<number> = [1,2,3]
+
